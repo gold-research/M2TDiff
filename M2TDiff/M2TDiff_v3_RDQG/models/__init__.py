a886af8ed25ac524c2fb4e4e8dfa395171eafd39
gold-research <goldplus040@gmail.com>
2026-08-29 14:44:39 +0800

    Delete M2TDiff directory

--- M2TDiff/M2TDiff_v3_RDQG/models/__init__.py
+++
@@ -1,1 +0,0 @@
-from .m2tdiff import M2TDiff
--- M2TDiff/datasets/imagenet_vid.py
+++
@@ -1,9 +0,0 @@
-from torch.utils.data import Dataset
-class ImageNetVIDDataset(Dataset):
-    def __init__(self, root, num_frames=4, transform=None):
-        self.root=root; self.num_frames=num_frames; self.transform=transform
-        # Add XML parsing / sequence indexing here according to local VID layout.
-        self.samples=[]
-    def __len__(self): return len(self.samples)
-    def __getitem__(self,idx):
-        raise NotImplementedError("Implement ImageNet VID sequence indexing and XML annotation parsing for your dataset layout.")
