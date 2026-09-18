2cad8f450cfbdc49dfdae99afa3f1d6f13e4b799
gold-research <goldplus040@gmail.com>
2026-09-18 15:03:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -102,9 +102,9 @@ def get_args_parser():
 
 
     # * Transformer
-    parser.add_argument('--enc_layers', default=6, type=int,
+    parser.add_argument('--enc_layers', default=4, type=int,
                         help="Number of encoding layers in the transformer")
-    parser.add_argument('--dec_layers', default=6, type=int,
+    parser.add_argument('--dec_layers', default=4, type=int,
                         help="Number of decoding layers in the transformer")
     parser.add_argument('--dim_feedforward', default=1024, type=int,
                         help="Intermediate size of the feedforward layers in the transformer blocks")
